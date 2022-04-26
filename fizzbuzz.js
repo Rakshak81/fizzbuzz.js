5a0b86c98bdb6344ed765033085e059eaea09fa8
Rakshak Shetty <rakshakshetty@Rakshaks-MacBook-Pro.local>
2022-04-26 11:44:38 +0100

    Buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,8 @@
 const fizzbuzz = (number) => {
   if( number % 3 === 0) {
       return 'Fizz';
+  } else if(number % 5 === 0) {
+      return 'Buzz';
   }
 }
 
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,6 +3,10 @@ describe('fizzbuzz', () => {
     it('fizzbuzz 3', () => {
         expect(fizzbuzz(3)).toBe('Fizz');
     });
-
-    
+   it('fizzbuzz 5', () => {
+       expect(fizzbuzz(5)).toBe('Buzz');
+   })
+   it('fizzbuzz 15', () => {
+       expe
+   })
 });
